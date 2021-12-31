08c0ad8c82c5ff152d18243a1ef1985e109b1126
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2021-12-31 17:24:23 +0700

    build: init

---
+++ src/shims.d.ts
@@ -0,0 +1,15 @@
+declare interface Window {
+  // extend the window
+}
+
+// with vite-plugin-md, markdowns can be treat as Vue components
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
+}
+
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
