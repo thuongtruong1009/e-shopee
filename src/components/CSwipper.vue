fba3a682bec1635283d879cc66c52a668bcd9280
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-01-05 23:52:04 +0700

    refactor: sizing

--- src/components/CSwipper.vue
+++ src/components/CSwipper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-w-screen-lg h-150">
+  <div class="min-w-screen-md h-150">
     <transition-group name="fade" tag="div">
       <div v-for="i in [currentIndex]" :key="i">
         <img :src="currentImg">
