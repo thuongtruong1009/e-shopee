014dd83cd7f8b9b68313e07da2c8bac39128c3fa
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-01-19 00:29:49 +0700

    add login for seller

---
+++ src/components/SHead.vue
@@ -0,0 +1,34 @@
+<script setup>
+import IShopee from '~/components/IShopee.vue'
+import IFacebook from '~/components/IFacebook.vue'
+import IInstagram from '~/components/IInstagram.vue'
+</script>
+
+<template>
+  <div class="seller-header-container h-13 flex justify-between items-center border-3 border-solid border-gray-200 border-opacity-60">
+    <router-link to="seller/login">
+      <div class="seller-header-left flex justify-around items-center text-xl tracking-tighter capitalize w-65 cursor-pointer">
+        <IShopee />
+        <h2>Seller channel</h2>
+      </div>
+    </router-link>
+    <div class="seller-header-right flex  justify-around items-center w-67 cursor-pointer">
+      <router-link to="/">
+        <h2 class="hover:underline text-sm font-medium">
+          Trang chủ Shopee
+        </h2>
+      </router-link>
+      <span class="opacity-20">|</span>
+      <IFacebook class="hover:bg-gray-200 p-1.5 rounded-full" />
+      <IInstagram class="hover:bg-gray-200 p-1.5 rounded-full" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.seller-header-container{
+  border-right: none;
+  border-left: none;
+  border-top: none;
+}
+</style>
---
+++ src/pages/seller/login.vue
@@ -0,0 +1,69 @@
+<route lang="yaml">
+meta:
+  layout: SLogin
+</route>
+
+<template>
+  <div class="seller-login-container grid grid-cols-2">
+    <div class="seller-login-container-left grid pt-10">
+      <h2 class="text-red-500 font-bold text-3xl py-2">
+        Professional seller
+      </h2>
+      <h6 class="text-lg tracking-tight w-[80%]">
+        Manage your shop more effectively on Shopee with Shopee - Seller Channel
+      </h6>
+      <img src="/img/seller/login/background.png" class="bg-contain w-md h-[13rem] mt-5">
+    </div>
+    <div class="seller-login-container-right flex justify-end">
+      <div class="w-[75%] bg-white grid justify-center p-5">
+        <h2 class="text-3xl font-semibold tracking-tight mb-5">
+          Login to Seller channel
+        </h2>
+        <form action="">
+          <div>
+            <input type="text" class="outline-none rounded-md border-1 border-solid border-gray-300 py-2 px-4 w-full focus:bg-[#E8F0FE] duration-200" placeholder="username or email...">
+          </div>
+          <div>
+            <input type="text" class="outline-none rounded-md border-1 border-solid border-gray-300 py-2 px-4 w-full focus:bg-[#E8F0FE] duration-200" placeholder="password...">
+          </div>
+          <div class="flex justify-between text-xs">
+            <div>
+              <input id="remember" type="checkbox" class="mr-2">
+              <label for="remember">Remember me</label>
+            </div>
+            <p class="text-blue-600 hover:text-blue-800 cursor-pointer font-medium">
+              Forgot password?
+            </p>
+          </div>
+          <div class="bg-red-500 text-center text-white rounded-md">
+            <button>Login</button>
+          </div>
+          <div class="grid gap-5">
+            <div class="flex justify-between text-xs pt-5">
+              <p class="opacity-60">
+                Don't have a Shopee account yet?
+              </p>
+              <p class="text-blue-600 hover:text-blue-800 font-medium">
+                Create account
+              </p>
+            </div>
+            <div class="flex justify-center opacity-40 text-xs">
+              <span>_____________________  OR  _____________________</span>
+            </div>
+            <!-- <span>OR</span> -->
+            <div class="text-center outline-none rounded-md border-1 border-solid border-gray-300 py-2 px-4">
+              <p>Login with Primary/Secondary account</p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+form > div{
+  padding: 0.5rem 0;
+  margin: 0.3rem 0;
+}
+</style>
