fbe0ad94e6e025a642b2ca082f347fb26b4edcc3
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-02-23 23:00:26 +0700

    rename pwa app

--- vite.config.ts
+++ vite.config.ts
@@ -105,8 +105,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'e-shopee',
+        short_name: 'e-shopee',
         theme_color: '#ffffff',
         icons: [
           {
