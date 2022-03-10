320c8da8e634b10f39050c2c0cc27299ba91c152
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-03-10 09:27:46 +0700

    render favourite from list

--- src/pages/favourite.vue
+++ src/pages/favourite.vue
@@ -2,20 +2,78 @@
 meta:
   layout: LFavourite
 </route>
+
+<script setup>
+const favouriteList = reacttive([{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+},
+{
+  img: '/img/today/1.jfif',
+  name: 'Bright Idea Moisturizer',
+  desc: 'Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer',
+  price: 24,
+}])
+</script>
+
 <template>
   <div class="favourite-container flex flex-row justify-center gap-5 flex-wrap">
-    <div v-for="i in 9" :key="i" class="grid justify-center items-center gap-3 rounded-lg shadow-md shadow-gray-400 bg-[#8CD0E3] p-2 w-65">
-      <img src="/public/img/today/1.jfif" alt="favourite_img" class="rounded-full w-60 h-60 justify-self-center">
+    <div v-for="(favourite, i) in favouriteList" :key="i" class="grid justify-center items-center gap-3 rounded-lg shadow-md shadow-gray-400 bg-[#8CD0E3] p-2 w-65">
+      <img :src="favourite.img" alt="favourite_img" class="rounded-full w-60 h-60 justify-self-center">
       <h6 class="text-sm font-medium">
-        Bright Idea Moisturizer
+        {{ favourite.name }}
       </h6>
       <p class="text-xs text-gray-500">
-        Vitamin C + Tri-Peptide Collagen Protecting & Brightening Moisturizer
+        {{ favourite.desc }}
       </p>
       <div class="flex justify-center gap-2">
         <button><IHeart /></button>
         <button class="flex items-center gap-2 btn bg-[#948AD3] rounded-md shadow-md shadow-gray-400">
-          Add to cart<span>$24</span>
+          Add to cart<span>${{ favourite.price }}</span>
         </button>
       </div>
     </div>
