86e13744e5803db6dd3a1627294814028a3d18ea
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-16 17:31:51 +0700

    feat: add address form

--- src/pages/buyer/account/address.vue
+++ src/pages/buyer/account/address.vue
@@ -1,3 +1,8 @@
+<route lang="yaml">
+meta:
+  layout: buyer/account/LBAddress
+</route>
+
 <template>
   <div class="myaccount-content border-1 border-solid border-light-700 rounded-md p-5 bg-[#EBF6FC]">
     <div class="border-b-1 border-b-solid border-b-light-700 py-3 font-medium flex items-center gap-1">
@@ -48,11 +53,34 @@
         <label>Address</label>
         <input type="text" name="address" required>
       </div>
+
+      <div class="pt-5 flex justify-end">
+        <button type="submit" class="btn bg-black hover:bg-[#F33535] duration-200 flex items-center gap-1 shadow-md shadow-gray-300 font-medium">
+          <ISave />Save Changes
+        </button>
+      </div>
     </form>
   </div>
 </template>
 
-<route lang="yaml">
-meta:
-  layout: buyer/account/LBAddress
-</route>
+<style scoped>
+input{
+  width: 80%;
+  outline: none;
+  border: 1px solid rgba(233, 236, 239);
+  border-radius: 0.3rem;
+  padding: 0.3rem 1rem;
+  transition: 0.2s linear;
+  font-size: 0.9rem;
+}
+input:focus{
+    box-shadow: 2px 2px 4px rgba(59, 175, 252, 0.8);
+}
+form > div:not(:last-child){
+  padding: 0.5rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+}
+</style>
