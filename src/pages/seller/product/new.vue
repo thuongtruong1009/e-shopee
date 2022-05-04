60f460f18c9ad89a88442f9527f6258b5b912ee8
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-05-04 17:40:14 +0700

    add transport infor

--- src/pages/seller/product/new.vue
+++ src/pages/seller/product/new.vue
@@ -6,10 +6,12 @@ meta:
 <template>
   <div class="seller_new_prod_view max-w-254 w-254">
     <div class="basic_infor p-7 my-5 bg-white shadow-md shadow-gray-300 rounded-xl">
-      <h5 class="font-medium text-lg">
-        Basic information
-      </h5>
-      <div class="fields flex w-full">
+      <div>
+        <h5 class="font-medium text-lg">
+          Basic information
+        </h5>
+      </div>
+      <div class="basic_fields flex w-full">
         <div>
           <label>Product image</label>
         </div>
@@ -32,7 +34,7 @@ meta:
           </div>
         </div>
       </div>
-      <div class="fields flex w-full">
+      <div class="basic_fields flex w-full">
         <div>
           <label>Product video</label>
         </div>
@@ -53,7 +55,7 @@ meta:
           </div>
         </div>
       </div>
-      <div class="fields flex w-full">
+      <div class="basic_fields flex w-full">
         <div>
           <label>* Product name</label>
         </div>
@@ -61,7 +63,7 @@ meta:
           <input type="text">
         </div>
       </div>
-      <div class="fields flex w-full">
+      <div class="basic_fields flex w-full">
         <div>
           <label>* Product descriptions</label>
         </div>
@@ -69,7 +71,7 @@ meta:
           <textarea id="" name="prod_desc" rows="10" />
         </div>
       </div>
-      <div class="fields flex w-full">
+      <div class="basic_fields flex w-full">
         <div>
           <label>* Category</label>
         </div>
@@ -92,7 +94,7 @@ meta:
           Fill full information to grow level of the product display. <a href="" class="text-blue-600 font-medium">Guide view</a>
         </p>
       </div>
-      <div class="detail_fields grid grid-cols-2 gap-5 mt-5">
+      <form method="post" class="detail_fields grid grid-cols-2 gap-5 mt-5">
         <div>
           <label>* Brand</label>
           <select name="brand">
@@ -147,26 +149,66 @@ meta:
             </option>
           </select>
         </div>
-      </div>
+      </form>
       <div />
+    </div>
+    <div class="transport_infor p-7 my-5 bg-white shadow-md shadow-gray-300 rounded-xl">
+      <div>
+        <h5 class="font-medium text-lg">
+          Transport
+        </h5>
+      </div>
+      <form method="post">
+        <div class="transport_fields">
+          <label>Weight (After packing)</label>
+          <div class="flex divide-x divide-solid divide-1 divide-[#e9e9e9]">
+            <input type="text" name="weight">
+            <p>gr</p>
+          </div>
+        </div>
+        <div class="transport_fields">
+          <label>Packing size (variable)</label>
+          <div class="flex divide-x divide-solid divide-1 divide-[#e9e9e9]">
+            <input type="text" name="width" placeholder="W">
+            <p>cm</p>
+          </div>
+          <div class="flex divide-x divide-solid divide-1 divide-[#e9e9e9]">
+            <input type="text" name="long" placeholder="L">
+            <p>cm</p>
+          </div>
+          <div class="flex divide-x divide-solid divide-1 divide-[#e9e9e9]">
+            <input type="text" name="height" placeholder="H">
+            <p>cm</p>
+          </div>
+        </div>
+        <div class="transport_fields">
+          <label>Fee</label>
+          <button class="px-5 py-2 rounded-md hover:bg-[#FFF5F1] border-1 border-solid border-[#EE4D2D] text-[#EE4D2D] text-sm">
+            Activate the transport unit
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="flex justify-end">
+      <button class="inline rounded-md bg-[#EE4D2D] hover:bg-[#E54A2B] px-3 py-1.5 text-white text-md flex items-center gap-1"><ISave />Save & Display</button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.fields{
+.basic_fields{
   margin-top: 1rem;
 }
-.fields > div:nth-child(1){
+.basic_fields > div:nth-child(1){
   width: 20%;
   text-align: right;
   padding-right: 1rem;
   font-size: .8em;
 }
-.fields > div:nth-child(2){
+.basic_fields > div:nth-child(2){
   width: 80%;
 }
-textarea, input{
+.basic_fields textarea, .basic_fields input{
   border: 1px solid #e9e9e9;
   width: 100%;
   border-radius: 0.5rem;
@@ -198,5 +240,30 @@ label{
 }
 select, option{
   padding: 0.3rem;
+}
+.transport_fields{
+  display: flex;
+  text-align: right;
+  align-items: ceter;
+  margin-top: 1rem;
+}
+.transport_fields > div{
+  display: flex;
+  border: 1px solid #e9e9e9;
+  padding: 0.2rem;
+  border-radius: 0.3rem;
+  margin-right: 1rem;
+}
+.transport_fields > div:hover, textarea:hover, select:hover{
+  border: 1px solid #cccccc;
+}
+
+.transport_fields input,
+.transport_fields p{
+  padding: 0.25rem;
+}
+.transport_fields > label{
+  width: 20%;
+  padding-right: 1rem;
 }
 </style>
