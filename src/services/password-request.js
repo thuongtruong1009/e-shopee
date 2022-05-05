deaff4af6efb4c1ef0256322cc60b2d9fba94f8b
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-05-05 22:46:52 +0700

    feat: add password request

--- src/services/password-request.js
+++ src/services/password-request.js
@@ -1,8 +1,34 @@
 import AxiosInstance from './axios-instance'
 
 class PasswordRequest {
-  passwordUser() {
+  updatePasswordUser(data) {
+    const url = '/password'
+    return AxiosInstance.put(url, data)
+  }
 
+  fogotPasswordUser(data) {
+    const url = '/password/forgot'
+    return AxiosInstance.post(url, data)
+  }
+
+  resetPasswordUser(data) {
+    const url = '/password/reset'
+    return AxiosInstance.post(url, data)
+  }
+
+  updatePasswordAdmin(data) {
+    const url = '/password/admin'
+    return AxiosInstance.put(url, data)
+  }
+
+  forgotPasswordAdmin(data) {
+    const url = '/password/admin/forgot'
+    return AxiosInstance.post(url, data)
+  }
+
+  resetPasswordAdmin(data) {
+    const url = '/password/admin/reset'
+    return AxiosInstance.post(url, data)
   }
 }
 export default new PasswordRequest()
