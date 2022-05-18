a8c8d8bc1b2acecfa95eab093c00cbae9966c50f
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-05-18 22:53:26 +0700

    add script locales

--- src/pages/seller/product/category.vue
+++ src/pages/seller/product/category.vue
@@ -10,6 +10,7 @@ import { useProduct } from '~/stores/product'
 useHead({
   title: 'seller | add product category',
 })
+const { t } = useI18n()
 const product = useProduct()
 const numberWord = ref('')
 
@@ -42,15 +43,15 @@ const getLevel3 = async(id) => {
   <div class="seller-prod-add-container max-w-300 w-300 p-7 bg-white shadow-sm shadow-gray-500/50 rounded-2xl divide-y divide-solid divide-1 divide-light-800">
     <div class="pb-7">
       <h4 class="text-xl font-medium my-1">
-        Add 1 new product
+        {{ t('category.title') }}
       </h4>
       <p class="text-gray-400 text-sm">
-        Please select the appropriate category for your product.
+        {{ t('category.desc') }}.
       </p>
     </div>
     <div>
       <div class="pt-8 mb-4 flex items-center">
-        <label for="add-product-name">Product name: </label>
+        <label for="add-product-name">{{ t('category.product-name') }}: </label>
         <div class="w-[85%] ml-5 border-1 border-red-500 border-solid flex items-center py-1 rounded-md divide-x divide-solid divide-3 divide-gray-300">
           <input id="add-product-name" v-model="numberWord" type="text" name="add-product-name" class="w-full outline-none px-2" placeholder="Input..." pattern="[A-Za-z0-9]{120}" required>
           <p class="flex justify-around opacity-60 text-xs px-2">
@@ -65,46 +66,20 @@ const getLevel3 = async(id) => {
             <input id="" type="text" name="add-product-search" class="outline-none ml-2 w-[85%]" placeholder="Product type name...">
           </div>
           <p class="text-gray-400 text-sm">
-            Choose the correct category, <a href="" class="text-blue-500">click here to find out</a>
+            {{ t('category.tutorial1') }}, <a href="" class="text-blue-500">{{ t('category.tutorial2') }}</a>
           </p>
         </div>
         <CSChooseCategory :level0="product.category.children" :level1="product.level1.children" :level2="product.level2.children" :level3="product.level3.children" @get-level1="getLevel1" @get-level2="getLevel2" @get-level3="getLevel3" />
-        <!-- <div class="bg-white grid grid-cols-4 divide-x divide-solid divide-3 divide-gray-300 mt-5">
-          <div class="py-2 max-h-80 overflow-y-scroll">
-            <div v-for="(category, i) in product.category.children" :key="i" class="flex justify-between items-center hover:bg-[#FAFAFA] px-3 py-1 cursor-pointer" @click="getLevel1(category.id)">
-              <p>{{ category.name }}</p>
-              <ICaretRight v-if="category.number_of_children" />
-            </div>
-          </div>
-          <div>
-            <div v-for="(category, i) in product.level1.children" :key="i" class="flex justify-between items-center hover:bg-[#FAFAFA] px-3 py-1 cursor-pointer" @click="getLevel2(category.id)">
-              <p>{{ category.name }}</p>
-              <ICaretRight v-if="category.number_of_children"/>
-            </div>
-          </div>
-          <div>
-            <div v-for="(category, i) in product.level2.children" :key="i" class="flex justify-between items-center hover:bg-[#FAFAFA] px-3 py-1 cursor-pointer" @click="getLevel3(category.id)">
-              <a>{{ category.name }}</a>
-              <ICaretRight v-if="category.number_of_children" />
-            </div>
-          </div>
-          <div>
-            <div v-for="(category, i) in product.level3.children" :key="i" class="flex justify-between items-center hover:bg-[#FAFAFA] px-3 py-1 cursor-pointer">
-              <p>{{ category.name }}</p>
-              <ICaretRight v-if="category.number_of_children" />
-            </div>
-          </div>
-        </div> -->
       </div>
       <div class="flex my-3 font-medium">
-        <p>👉 Selected: </p>
+        <p>👉 {{ t('category.selected') }}: </p>
         <p class="text-red-500 ml-5">
           {{ product.choicedList[0].name }} <span v-if="product.choicedList[1].name ">></span> {{ product.choicedList[1].name }} <span v-if="product.choicedList[2].name ">></span> {{ product.choicedList[2].name }}
         </p>
       </div>
       <router-link to="/seller/product/new">
         <button class="bg-[#E54A2B] text-white rounded-md px-10 py-1.5 my-5 shadow-md shadow-gray-300">
-          Next
+          {{ t('category.next') }}
         </button>
       </router-link>
     </div>
